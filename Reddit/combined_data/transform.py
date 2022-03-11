a6d2fa0da617e5855c15627d28a66885b755ea23
Brandonchriss <bkretser.2020@sis.smu.edu.sg>
2022-03-11 20:47:26 +0800

    cleaned_data

---
+++ Reddit/combined_data/transform.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+#filename = "Compound"
+#filename = "MakerDAO"
+filename = "Curve Finance"
+
+extension = filename + "_combined.csv"
+
+df = pd.read_csv(extension)
+
+print(df.describe())
+
+#Remove duplicate titles
+df_dropdup = df.drop_duplicates(subset=['title'])
+
+print(df_dropdup.describe())
+
+#Remove empty body
+df_dropempty = df_dropdup.dropna(subset=['body'])
+
+print(df_dropempty.describe())
+
+#remove [deleted]
+df_dropdeleted = df_dropempty[df_dropempty['body'] != '[deleted]']
+print(df_dropdeleted.count())
+
+#remove [removed]
+df_dropremoved = df_dropdeleted[df_dropdeleted['body'] != '[removed]']
+print(df_dropremoved.count())
+
+df_dropremoved.to_csv(filename + "_combined_cleaned.csv", index=False, header=True)
