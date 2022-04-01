d587acda8f0ebfd7b50ac7f14677919fca9b0250
Caleb Cheong <calebcheongdnt@gmail.com>
2022-04-01 19:47:36 +0800

    asd

--- Dashboard/finance-charts.py
+++ Dashboard/finance-charts.py
@@ -7,97 +7,34 @@ import plotly.express as px
 # for sentiment analysis
 import numpy as np
 import re
-from nltk.corpus import stopwords
-import nltk
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from datetime import datetime
 from plotly.subplots import make_subplots
 import dash_daq as daq
 from dash import html
 import json
 
-def sentiment_analysis(chosen_defi_coin):
-    stop = stopwords.words('english')
-    def clean_content(contentInput):
-        if type(contentInput) == np.float:
-            return ""
-        contentInput = contentInput.lower()
-        contentInput = re.sub("'", "", contentInput) # to avoid removing contractions in english
-        contentInput = re.sub("@[A-Za-z0-9_]+","", contentInput)
-        contentInput = re.sub("#[A-Za-z0-9_]+","", contentInput)
-        contentInput = re.sub(r'http\S+', '', contentInput)
-        contentInput = re.sub('[()!?]', ' ', contentInput)
-        contentInput = re.sub('\[.*?\]',' ', contentInput)
-        contentInput = re.sub("[^a-z0-9]"," ", contentInput)
-        contentInput = contentInput.strip()
-        contentInput = contentInput.split()
-        contentInput = [w for w in contentInput if not w in stop]
-        contentInput = " ".join(word for word in contentInput)
-        return contentInput
 
-    string = chosen_defi_coin.lower() + ".csv"
-    df = pd.read_csv(string)
-
-    df['date'] = df['created'].apply(lambda x: x.split(' ')[0])
-    df.sort_values(['date'], inplace=True)
-
-    df['year_month'] = df['date'].apply(lambda x: x[0:x.rfind('-')])
-
-    df = df.fillna(method='ffill')
-    # monthList = pd.date_range('2021-01-01','2022-02-01', 
-    #             freq='MS').strftime("%m/%Y").tolist()
-    monthList = list(df['year_month'].unique())
-
-    timeSplitData={}
-    timeSplitDataSentiment = {}
-
-    for month in monthList: 
-        timeSplitData[month] = []
-        timeSplitDataSentiment[month] = []  
-
-    for row in df.iterrows():
-        month = row[1]['year_month']
-        content = row[1]['body']
-        cleaned = clean_content(content)
-        timeSplitData[month].append(cleaned)
-
-    sid = SentimentIntensityAnalyzer()
-
-    for month in monthList: # for month in timeSplitData:
-        for content in timeSplitData[month]:
-            output = sid.polarity_scores(content)
-            timeSplitDataSentiment[month].append(output)
-    
-    timeSplitDataNetScore = {}
-    for month in monthList:
-        timeSplitDataNetScore[month] = 0 
-        for output in timeSplitDataSentiment[month]:
-            compound = output['compound']
-            compound = compound / len(timeSplitDataSentiment[month])
-            timeSplitDataNetScore[month] +=compound
-
-    return timeSplitDataNetScore
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.LUMEN])
 # app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
+
 @callback(
     Output(component_id='price-chart', component_property='figure'),
-    Output(component_id='chosen-defi-coin', component_property='children'), 
-    Input(component_id='defi-coin', component_property='value'), ###here
+    Output(component_id='chosen-defi-coin', component_property='children'),
+    Input(component_id='defi-coin', component_property='value'),  # here
     Input(component_id='my-toggle-switch', component_property='value'),
 )
-
-#chosen_volume
-def build_graphs(chosen_defi_coin, match): ###here
+# chosen_volume
+def build_graphs(chosen_defi_coin, match):  # here
     string = chosen_defi_coin.lower() + "_price_data.csv"
     price_df = pd.read_csv(string)
     # fig = px.line(df, x="Date", y="Price", template="plotly_dark")
 
-    # here 
+    # here
     # timeSplitDataNetScore = sentiment_analysis(chosen_defi_coin.lower())
     # timeSplitDataNetScore = pd.read_json('sentimentalOutput/discord.json')
-    new_dict= {}
+    new_dict = {}
     string = 'sentimentalOutput/discord-' + chosen_defi_coin.lower() + '.json'
     with open(string) as json_file:
         timeSplitDataNetScore = json.load(json_file)
@@ -106,7 +43,8 @@ def build_graphs(chosen_defi_coin, match): ###here
             new_key = arr[1] + "-" + arr[0]
             new_dict[new_key] = timeSplitDataNetScore[old_key]
 
-    sentiment_df = pd.DataFrame({'date': new_dict.keys(), 'sentiment': new_dict.values()})
+    sentiment_df = pd.DataFrame(
+        {'date': new_dict.keys(), 'sentiment': new_dict.values()})
 
     def mdy_to_ymd(d):
         return datetime.strptime(d, '%b %d, %Y').strftime('%Y-%m-%d')
@@ -114,17 +52,20 @@ def build_graphs(chosen_defi_coin, match): ###here
     price_df['format_date'] = price_df['Date'].apply(lambda x:  mdy_to_ymd(x))
 
     fig = make_subplots(specs=[[{"secondary_y": True}]])
-    fig.add_trace(go.Scatter(x=price_df['format_date'], y=price_df['Price'], name='Price'), secondary_y= False)
-    fig.update_xaxes(autorange="reversed") 
+    fig.add_trace(go.Scatter(
+        x=price_df['format_date'], y=price_df['Price'], name='Price'), secondary_y=False)
+    fig.update_xaxes(autorange="reversed")
     # fig.update_xaxes(dtick="M1")
 
     if match == False:
         chosen_defi_coin = "End"
-        fig.add_trace(go.Scatter(x=sentiment_df['date'], y=sentiment_df['sentiment'], name='Sentiment', xperiodalignment='end'), secondary_y= True)
+        fig.add_trace(go.Scatter(x=sentiment_df['date'], y=sentiment_df['sentiment'],
+                      name='Sentiment', xperiodalignment='end'), secondary_y=True)
 
     if match == True:
         chosen_defi_coin = "Start"
-        fig.add_trace(go.Scatter(x=sentiment_df['date'], y=sentiment_df['sentiment'], name='Sentiment', xperiodalignment='start'), secondary_y= True)
+        fig.add_trace(go.Scatter(x=sentiment_df['date'], y=sentiment_df['sentiment'],
+                      name='Sentiment', xperiodalignment='start'), secondary_y=True)
 
     fig.update_layout(
         xaxis_rangeslider_visible=True,
@@ -134,19 +75,21 @@ def build_graphs(chosen_defi_coin, match): ###here
     fig.update_xaxes(title_text="Date")
     fig.update_yaxes(title_text="Price", secondary_y=False)
     fig.update_yaxes(title_text="Sentiment", secondary_y=True)
-    ## here
+    # here
 
     # fig.update_traces(line_color='#FF0000')
     chosen_defi_coin = chosen_defi_coin + " price data"
     fig.update_yaxes(autorange="reversed")
     return fig, chosen_defi_coin
 
+
 app.layout = dbc.Container([
     html.H1(id='chosen-defi-coin', style={'textAlign': 'center'}),
-
+    dbc.Link("back", href=f"url"),
+    html.Button(dcc.Link("back", href=f"url"), className='three columns'),
     dbc.Row([
         dbc.Col(dcc.Dropdown(
-            id='defi-coin', ###here
+            id='defi-coin',  # here
             options=[
                 {'label': 'AAVE', 'value': 'AAVE'},
                 {'label': 'UNISWAP', 'value': 'UNISWAP'},
@@ -155,7 +98,7 @@ app.layout = dbc.Container([
                 {'label': 'SUSHI', 'value': 'SUSHI'},
                 {'label': 'COMPOUND', 'value': 'COMPOUND'},
             ],
-            value = 'AAVE'), width=dict(size=8, offset=2)),
+            value='AAVE'), width=dict(size=8, offset=2)),
 
         dbc.Col(
             daq.ToggleSwitch(
@@ -163,14 +106,15 @@ app.layout = dbc.Container([
                 value=False
             ),
         )
-            
-]),
+
+    ]),
 
     dbc.Row([
-        dbc.Col(dcc.Graph(id='price-chart')) 
-    ]), 
+        dbc.Col(dcc.Graph(id='price-chart'))
+    ]),
 ], fluid=True)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     # app.run_server(debug=True)
     app.run_server(debug=True, host='0.0.0.0', port=80)
