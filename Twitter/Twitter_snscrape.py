2894825a8f91f92d178fbb7d129bc84d6554bb93
yyyanwee <89122919+yyyanwee@users.noreply.github.com>
2022-04-01 17:50:12 +0800

    scrape

--- Twitter/Twitter_snscrape.py
+++ Twitter/Twitter_snscrape.py
@@ -1,52 +1,49 @@
 import enum
-import snscrape.modules.twitter as sntwitter
 import pandas as pd
+import numpy as np
 
+import snscrape.modules.twitter as sntwitter
 from datetime import datetime
 # keyword scraping
 # https://betterprogramming.pub/how-to-scrape-tweets-with-snscrape-90124ed006af
 # https://github.com/MartinBeckUT/TwitterScraper/blob/master/snscrape/python-wrapper/snscrape-python-wrapper.py
 
-tweets = []
-
-'''
-Defi List:
-makerDAO - $MKR
-compound - $COMP
-curve finance - $CRV
-aave - $AAVE
-uniswap - $UNI
-sushiswap - $SUSHI
-'''
-search_term = "BTC"
-
-# DONE : All done
-
 start_time = datetime.now()
-print("Started Scraping at: " + str(start_time))
-try:
-    for i,tweet in enumerate(sntwitter.TwitterSearchScraper(search_term + ' since:2021-09-01 until:2022-03-12').get_items()):
-        # tweet is literally a link to said tweet
-        content = tweet.content
-        # Remove line breaks 
-        content = content.replace('\n', ' ').replace('\r', ' ')
-        tweets.append([tweet.date, tweet.id, content, tweet.user.username])
-        if i % 10000 == 0:
-            print("Progress : {}, Time : {} ".format(i, datetime.now() - start_time))
-except KeyboardInterrupt:
-    print("Stopping Scraping....")
-    pass
+end = start_time.strftime("%Y-%m-%d")
+#END is the END of the date range needed for scraping
+# START is the START of the date range needed for scraping
 
-finally:
-    # convert to pandas DataFrame
-    tweets_df = pd.DataFrame(tweets, columns=['Date', 'ID', 'Content', 'Username'])
+# Start should be automatically assigned as the latest date of scraping for the data
 
-    print("Building CSV....")
+def scrape(search_term, start, end = end):
+    tweets = []
+    print("Started on : {}".format(start_time))
+    timeframeString = " since:" + start +" until:" + end
 
-    #convert DataFrame to CSV
-    tweets_df.to_csv('BTCTweets.csv', sep=',', index=False)
+    try:
+        for i,tweet in enumerate(sntwitter.TwitterSearchScraper(search_term + timeframeString).get_items()):
+            # tweet is literally a link to said tweet
+            content = tweet.content
+            # Remove line breaks 
+            content = content.replace('\n', ' ').replace('\r', ' ')
+            tweets.append([tweet.date, tweet.id, content, tweet.user.username])
+            if i % 10000 == 0:
+                print("Progress : {}, Time : {} ".format(i, datetime.now() - start_time))
+    except KeyboardInterrupt:
+        print("Stopping Scraping....")
+        # continue process
+        pass
+    finally:
+        # create dataframe
+        tweets_df = pd.DataFrame(tweets, columns=['Date', 'ID', 'Content', 'Username'])
+        print("Building CSV....")
 
-    end_time = datetime.now()
+        #convert DataFrame to CSV
+        tweets_df.to_csv('{}.csv'.format(search_term))
 
-    print("Ended Scraping at: " + str(end_time))
-    print("Duration: {}".format(end_time - start_time))
+        end_time = datetime.now()
+        print("Ended Scraping at: " + str(end_time))
+        print("Duration: {}".format(end_time - start_time))
+
+
+scrape("$aave", "2021-01-01", "2022-03-01")
